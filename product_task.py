539fafd7c9cff229c896d5a1cbb4428d242238da
Ar3019 <134208364+Ar3019@users.noreply.github.com>
2023-05-21 19:07:50 +0530

    product_task.py

--- product_task.py
+++ product_task.py
@@ -3,4 +3,8 @@
 from math import ceil
 
 def getInput():
+  items, wraps = [], []
   
+  for i in range(3):
+    items.append(int(input(f"No of units required for product {chr(65 + i)}: ")))
+    wraps.append(input("Should be gift wrapped (yes/no): "))
