f9f347881271a092235794472a71955f247f4045
Arundhathi B <134208364+Ar3019@users.noreply.github.com>
2024-02-26 18:58:11 +0530

    Update numberset_validation.py

--- numberset_validation.py
+++ numberset_validation.py
@@ -1,1 +1,22 @@
+# code for validating a set of nnumbers using array
 
+import array
+
+def validateNum(min_num, max_num, disabled_num, arr):
+    for i in range(len(arr)):
+        if (arr[i]<min_num) or (arr[i]>max_num) :
+            print ("Invalid Number:", arr[i])
+        elif arr[i] not in disabled_num:
+            print ("Valid number:", arr[i])
+         
+        else:
+            arr[i]+=1
+
+def main():
+    min_num = int(input("Enter the minimum number:"))
+    max_num = int(input("Enter maximum number:"))
+    disabled_num =set(map(int, input("Enter the disabled numbers: ").split()))
+    arr = [1, 5, 7, 8, 9, 12]
+    validateNum(min_num, max_num, disabled_num, arr)
+
+main()
